499e387b6e1a45d5ce1ac62830a7d4a528f55364
Ian Layzer <ianlayzer@gmail.com>
2021-01-12 07:34:55 -0500

    Now working with slack block ui

--- main.py
+++ main.py
@@ -1,6 +1,35 @@
 from piazza_api import Piazza
-from bs4 import BeautifulSoup
+from markdownify import markdownify
 import os
+
+def create_slack_message(piazza_post):
+    parent_history = piazza_post['history']
+    most_recent_parent_post = parent_history[len(parent_history) - 1]
+    message = {
+        'response_type': 'in_channel',
+        'blocks': [
+            {
+                'type': 'header',
+                'text': {
+                    'type': 'plain_text',
+                    'text': most_recent_parent_post['subject'],
+                    'emoji': True
+                }
+            },
+            {
+                'type': 'divider'
+            },
+            {
+                'type': 'section',
+                'text': {
+                    'type': 'mrkdwn',
+                    'text': markdownify(most_recent_parent_post['content'])
+                }
+            }
+        ]
+    }
+    return message
+
 
 def get_link(question_num):
     return os.environ.get('PIAZZA_BASE_URL') + str(question_num)
@@ -10,9 +39,7 @@ def get_content(question_num):
     p.user_login(email=os.environ.get('PIAZZA_EMAIL'), password=os.environ.get('PIAZZA_PASSWORD'))
     cs33 = p.network("keslbz8fxy144e")
     post = cs33.get_post(question_num)
-    html_content = post['history'][0]['content']
-    soup = BeautifulSoup(html_content, features="html.parser")
-    return soup.get_text()
+    return create_slack_message(post)
 
 def handle_input(input):
     tokens = input.split()
@@ -26,4 +53,4 @@ def handle_input(input):
 def piazza_bot(request):
     return handle_input(request.form['text'])
 
-print(handle_input('get 343'))
+print(handle_input('get 388'))
